88889f62c12f4304602a865e1f1049ea58a76541
Priyanshi <56130372+priyanshitaneja@users.noreply.github.com>
2020-05-15 13:07:25 +0530

    Add files via upload

---
+++ restaurantReg.php
@@ -0,0 +1,21 @@
+<?php include 'include/header.php' ?>
+    <div class="main-container">
+      <div class="container-left">
+        <form class="reg-form">
+          <h1 class="reg-h1">Register your Restaurant</h1>
+          <input type="text" placeholder="Email">
+          <input type="password" placeholder="Password">
+          <input type="password" placeholder="Confirm Password">
+          <input type="text" placeholder="Address">
+          <button class="btn1 reg-btn">
+            Register
+          </button>
+        </form>
+      </div>
+      <div class="container-right">
+        <!-- <p>We care for you!</p> -->
+        <img src="./bg.jpg" alt="background-image" class="bg" />
+      </div>
+    </div>
+  </body>
+</html>
